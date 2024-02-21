78dcbcb4a6461cc9a9573d6add78cf7cf953d25b
SebastianD9 <107563772+SebastianD9@users.noreply.github.com>
2024-02-21 17:15:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const submit = document.querySelector('.Submit');
+const displayThank = document.querySelector('.thank');
+const rating = document.querySelector('.rating');
+const vote = document.querySelectorAll('.vote');
+let number = document.querySelector('#number');
+
+
+
+function thank() {
+    displayThank.style.display='flex';
+    rating.style.display='none';
+
+}
+function result(e) {
+    
+    vote.forEach(a => {
+        a.style.background = 'var(--Dark-Blue)';
+        a.style.color = 'var(--Medium-Grey)';
+    })
+       
+    console.log(e.target.closest('button'));
+    NUMBERR = e.srcElement.innerText;
+    number.innerHTML = NUMBERR;
+    console.log(NUMBERR);
+    e.target.closest('button').style.background = 'var(--Light-Grey)';
+    e.target.closest('button').style.color = 'var(--white)';
+}
+
+vote.forEach(e => {
+    e.addEventListener('click', result);
+});
+submit.addEventListener('click', thank);
+
